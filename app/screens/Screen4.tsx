3e77a4c4b6fc4b632722cdd4c6745924a29de9bb
Mohamed Kamal <medoking91@gmail.com>
2023-08-15 13:05:32 +0300

    feat: screen 2

--- app/screens/Screen4.tsx
+++ app/screens/Screen4.tsx
@@ -1,13 +1,63 @@
 // قطار الذهاب
+import {
+  faArrowRightArrowLeft,
+  faCheckCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
 
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 
-export const Screen4 = ({navigation}: any) => {
+export const Screen4 = () => {
   return (
-    <>
-      <View style={styles.container} />
-    </>
+    <ScrollView style={{backgroundColor: 'white'}}>
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginRight: 20,
+            }}>
+            <Text style={styles.text}>Passenger Name: {'محمد كمال'}</Text>
+            <FontAwesomeIcon icon={faCheckCircle} size={20} color={'#00798c'} />
+          </View>
+          <Text style={styles.subText}>الهوية الوطنية - 12345678</Text>
+          <Text style={styles.subText}>Saudi Arabia</Text>
+          <Text style={styles.subText}>بالغ 2001/5/20</Text>
+        </View>
+        <View style={styles.card}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginRight: 20,
+            }}>
+            <Text style={styles.text}>ادخل معلومات المسافر - شباب</Text>
+            <FontAwesomeIcon
+              icon={faArrowRightArrowLeft}
+              size={20}
+              color={'#00798c'}
+            />
+          </View>
+        </View>
+        <View style={styles.card}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginRight: 20,
+            }}>
+            <Text style={styles.text}>ادخل معلومات المسافر - طفل</Text>
+            <FontAwesomeIcon
+              icon={faArrowRightArrowLeft}
+              size={20}
+              color={'#00798c'}
+            />
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
@@ -16,26 +66,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
-  detailsContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-    alignItems: 'center',
-    position: 'absolute',
-  },
-  cityText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'grey',
-    textAlign: 'center',
-    marginHorizontal: 5,
-  },
-  cityText2: {
+  text: {
     fontSize: 16,
-    fontWeight: 'normal',
+    fontWeight: 'bold',
+    padding: 10,
+    color: '#00798c',
   },
-  image: {
-    width: 200,
-    resizeMode: 'contain',
+  subText: {
+    fontSize: 16,
+    marginRight: 20,
+    color: 'grey',
+    marginVertical: 5,
+  },
+  card: {
+    width: '90%',
+    paddingVertical: 30,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    shadowColor: '#000',
+    elevation: 5,
   },
 });
